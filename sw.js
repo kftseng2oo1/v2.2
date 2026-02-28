79a241ccc967105939d3db4f13570e9080c7f027
kftseng2oo1 <kftseng2oo1@gmail.com>
2026-02-28 22:21:40 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,69 @@
+const CACHE_NAME = 'iching-pwa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// 安裝 Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('快取已開啟');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  // 強制啟用新的 Service Worker
+  self.skipWaiting();
+});
+
+// 攔截網路請求
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 快取命中 - 返回快取的資源
+        if (response) {
+          return response;
+        }
+        
+        // 克隆請求
+        const fetchRequest = event.request.clone();
+        
+        return fetch(fetchRequest).then(response => {
+          // 檢查是否為有效回應
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          
+          // 克隆回應
+          const responseToCache = response.clone();
+          
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+          
+          return response;
+        });
+      })
+  );
+});
+
+// 更新 Service Worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  // 立即控制所有頁面
+  return self.clients.claim();
+});
